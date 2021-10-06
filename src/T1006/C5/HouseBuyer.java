016948e11d8bd752a449d8fd2859900273c75630
FCReSiA <348588013@qq.com>
2021-10-06 22:56:58 +0800

    C2.1&C2.2

---
+++ src/T1006/C5/HouseBuyer.java
@@ -0,0 +1,9 @@
+package T1006.C5;
+
+public class HouseBuyer {
+
+    public void buy() {
+        System.out.println("要买房");
+    }
+
+}
---
+++ src/T1006/C5/HouseOwner.java
@@ -0,0 +1,11 @@
+package T1006.C5;
+
+import java.util.logging.Logger;
+
+public class HouseOwner implements IHouseOwner {
+
+    @Override
+    public void tradeHouse(Logger log) {
+        log.info("房屋交易");
+    }
+}

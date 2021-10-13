8ab6cc690f374d799ad3b4e311d5e8d3412c132e
FCReSiA <348588013@qq.com>
2021-10-13 21:34:44 +0800

    C2.1&C2.2

--- src/D1013/AccountDao.java
+++ src/D1013/AccountDao.java
@@ -3,4 +3,8 @@ import D1013.Account;
 import java.util.List;
 public interface AccountDao {
     public  int addAccount(Account account);
+    public int deleteAccount(int i);
+    public Account findAccountbyid(int i);
+    public List<Account> findAllAccount();
+    public int updateAccount(Account account);
 }
